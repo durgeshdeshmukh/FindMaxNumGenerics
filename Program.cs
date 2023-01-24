2d88470d428d62ad63a399acfe4b77339a559a41
durgeshdeshmukh <durgesh.deshmukh2@gmail.com>
2023-01-23 21:44:27 -0800

    Find max of three strings

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Find max number");
+            string output = UC3_FindMaxOfThreeStrings.MaximumStringNumber("11", "22", "33");
+            Console.WriteLine(output);
         }
     }
 }
