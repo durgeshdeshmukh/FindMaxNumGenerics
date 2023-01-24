1dc7b8eab54242d895b628a179366c3aa6f4deda
durgeshdeshmukh <durgesh.deshmukh2@gmail.com>
2023-01-23 20:48:06 -0800

    FindMaxNumGenerics

--- Program.cs
+++ Program.cs
@@ -4,9 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Find max number using Generics");
-            int output = UC1_FindMaxOfThreeIntegers.MaximumIntegerNumber(11, 22, 33);
-            Console.WriteLine(output);
+            
         }
     }
 }
