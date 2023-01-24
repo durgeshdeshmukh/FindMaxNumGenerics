870ee6f613c94ecd4a9dcf06f2e1d83c6fc54131
durgeshdeshmukh <durgesh.deshmukh2@gmail.com>
2023-01-23 20:43:16 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FindMaxNumGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find max number using Generics");
+            int output = UC1_FindMaxOfThreeIntegers.MaximumIntegerNumber(11, 22, 33);
+            Console.WriteLine(output);
+        }
+    }
+}
