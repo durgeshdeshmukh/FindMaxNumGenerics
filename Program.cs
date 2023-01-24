cbaa6b809fda25fe6e654593e543d83f663c6f9b
durgeshdeshmukh <durgesh.deshmukh2@gmail.com>
2023-01-23 21:31:47 -0800

    Find max of three floats

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Find max number");
+            float output = UC2_FindMaxOfThreeFloats.MaximumFloatNumber(111.2F, 22.45F, 78.56F);
+            Console.WriteLine(output);
         }
     }
 }
