663085796e6b6a8112fb3647ce41e37ba3e975cd
durgeshdeshmukh <durgesh.deshmukh2@gmail.com>
2023-01-23 21:02:05 -0800

    find max integer num

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Find max integer number");
+            int output = UC1_FindMaxOfThreeIntegers.MaximumIntegerNumber(11, 22, 33);
+            Console.WriteLine(output);
         }
     }
 }
